c8d225111d02f98d147e54760e586bb08f8a825f
Jessica <jchung722@gmail.com>
2016-10-26 07:07:08 -0700

    fixed typo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,52 +1,59 @@
 class TasksController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_action :get_current_user
 
   def index
-    @tasks = Task.all
+    if @user
+      @tasks = @user.tasks
+    end
   end
 
   def create
     @mytask = Task.new(completed: false)
     @mytask.name = params[:task][:name]
+    @mytask.user_id = session[:user_id]
     @mytask.description = params[:task][:description]
     @mytask.save
-    redirect_to action: 'index'
+    redirect_to root_path
   end
 
   def destroy
     @mytask = Task.find(params[:id])
     @mytask.destroy
-    redirect_to action: 'index'
+    redirect_to root_path
   end
 
   def edit
     @mytask = Task.find(params[:id])
-    @path = "update"
+    if @mytask.user.id != session[:user_id]
+      redirect_to root_path
+    end
   end
 
   def new
     @mytask = Task.new
-    @path = "create"
   end
 
   def show
     @mytask = Task.find(params[:id])
-
-    if @mytask == nil
-      render :file => 'public/404.html',
-        :status => :not_found
+    if @mytask.user.id != session[:user_id]
+      redirect_to root_path
     end
-
   end
 
   def update
     Task.update(params[:id], :name => params[:task][:name], :description => params[:task][:description])
-    redirect_to show_path
+    redirect_to task_path
   end
 
   def complete
     Task.update(params[:id], :completed => true, :completed_at => Time.now)
     redirect_to root_path
+  end
+
+  private
+  def get_current_user
+    @user = User.find_by(id: session[:user_id])
   end
 
 end
